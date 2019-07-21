94415bad2488e3be08e5fc162adde1e3f7422838
Mallikarjuna Reddy <Mallikarjuna Reddy@Arjun>
2019-07-21 22:04:05 +0530

    - initial commit

---
+++ src/main/java/com/arjun/springboot/SpringBootSelfLearningApplication.java
@@ -0,0 +1,13 @@
+package com.arjun.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSelfLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSelfLearningApplication.class, args);
+	}
+
+}
